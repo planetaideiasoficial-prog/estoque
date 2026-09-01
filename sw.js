7246a1e1295f62121dc49b620b8ae05e96b806fa
planetaideiasoficial-prog <planetaideiasoficial@gmail.com>
2026-08-31 21:28:04 -0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'estoquepro-saas-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Retorna do cache se encontrar, senão vai para a internet
+        return response || fetch(event.request);
+      })
+  );
+});
